8e3dab281e07394456fcb3c324e0df5bfef4b058
J3Cs <joeybrook235@gmail.com>
2020-04-21 23:14:10 -0600

    Horner terminado

--- modulos/Horner.py
+++ modulos/Horner.py
@@ -10,9 +10,6 @@ def Horner(func, Es, limitA, limitB, xi):
     R = 0.0
     S = 0.0
     coeficientes = coefs(fun)
-    grado = len(coeficientes)-1
-    m = math.fabs(coeficientes[0])
-    n = math.fabs(coeficientes[grado])
     limitA = limitA
     limitB = limitB
     for t in range(len(coeficientes)):
@@ -129,7 +126,7 @@ def Horner(func, Es, limitA, limitB, xi):
             S = arr1[j]
         xi1 = xi - (R/S)
         Ea = math.fabs((xi1-xi)/xi1)*100
-        
+
 #Metodo para obtener los coeficientes de la funcion pasada como String
 def coefs(entrada):
   regexp = r"(-?\d*)(x?)(?:(?:\^|\*\*)(\d))?"
